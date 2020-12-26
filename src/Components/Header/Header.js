c0965ea3c85e1c41bb5cb0594b29d330d3745fd0
saaketh <saakethaj@gmail.com>
2020-12-26 15:29:01 +0530

    unused functions removed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,13 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import "./Header.css";
 
-function setCookie(cname, cvalue, exdays) {
-  var d = new Date();
-  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-  var expires = "expires=" + d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
 function Header() {
   const history = useHistory();
 
