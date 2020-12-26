e8a4af22ee86261f108a88d987b9ef722abd5ebc
saaketh <saakethaj@gmail.com>
2020-12-26 13:37:01 +0530

    blog tile handled and landing page made dynamic

--- src/Components/BlogTile/BlogTile.js
+++ src/Components/BlogTile/BlogTile.js
@@ -3,26 +3,34 @@ import React from "react";
 import "./BlogTile.css";
 
 function BlogTile(props) {
+  const { state, initFetch } = props;
+
+  const handleDelete = () => {
+    fetch(`http://localhost/delete.php?id=${state.id}`, {
+      method: "DELETE",
+    }).then(() => {
+      initFetch(Math.random());
+    });
+  };
+
   return (
     <div className={props.admin ? "BlogTile BlogTile__minHeight" : "BlogTile"}>
-      <div className="BlogTile__header BlogTile__contentTile">Code</div>
+      <div className="BlogTile__header BlogTile__contentTile">
+        {state.title}
+      </div>
       <div className="BlogTile__body BlogTile__contentTile">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure recusandae
-        maiores enim sed quibusdam quidem reiciendis repellat veniam expedita
-        voluptas aperiam, natus laudantium suscipit voluptate, laborum beatae
-        optio vel nisi? Lorem ipsum dolor sit, amet consectetur adipisicing
-        elit. Asperiores laboriosam eius et dolor dolores quo, qui vero animi
-        maiores obcaecati voluptate consequuntur enim totam perferendis! Placeat
-        nesciunt fugiat molestiae similique? Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Blanditiis, ducimus deleniti. Obcaecati
-        eos magni harum molestiae rem, similique cupiditate quae? Ut officiis
-        saepe ex quisquam nihil eos accusantium. Sapiente, nobis.
+        {state.description}
+      </div>
+      <div className="BlogTile__contentTile BlogTile__author">
+        <p>- {state.author}</p>
       </div>
       <hr />
       {props.admin ? (
         <div className="float-right">
           <Button variant="warning">Edit</Button>
-          <Button variant="danger">Delete</Button>
+          <Button variant="danger" onClick={handleDelete}>
+            Delete
+          </Button>
         </div>
       ) : null}
     </div>
--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import BlogTile from "../BlogTile";
 import "./Landing.css";
 
 function Landing(props) {
+  const [isactive, setActive] = useState(true);
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    setActive(true);
+    fetch("http://localhost/get.php")
+      .then((res) => res.json())
+      .then((data) => {
+        if (isactive) {
+          setPosts(data);
+        }
+      });
+    return () => {
+      setActive(false);
+    };
+  }, []);
+
+  console.log(posts);
+
   return (
     <div className="Landing">
       <Container fluid>
         <Row>
           <Col md={10} className="Landing__BlogTile">
-            <BlogTile {...props} />
-            <BlogTile />
-            <BlogTile />
-            <BlogTile />
-            <BlogTile />
+            {posts.map((value, index) => {
+              return <BlogTile {...props} key={index} state={value} />;
+            })}
           </Col>
         </Row>
       </Container>
